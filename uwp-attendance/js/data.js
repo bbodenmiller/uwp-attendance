e498b5c5d71c01f235b0cf1415507522a38c5889
Ben Bodenmiller <bbodenmiller@hotmail.com>
2016-05-09 21:53:48 -0700

    don't save to file if already checked in
    fix #6

--- uwp-attendance/js/data.js
+++ uwp-attendance/js/data.js
@@ -1,20 +1,48 @@
 ﻿'use strict';
 
-function saveCheckin(personID, secureID) {
-    var personInfo = personID;
+var applicationData = Windows.Storage.ApplicationData.current;
+var localFolder = applicationData.localFolder;
+var filename = "checkins.csv";
 
-    if (secureID) {
-        personInfo += ", " + secureID;
+function checkedInStatus(personID, secureID) {
+    if (personID) {
+        personID = personID.toLowerCase() + ",";
     }
-
-    //todo: if already checked in don't save again
-
-    var applicationData = Windows.Storage.ApplicationData.current;
-    var localFolder = applicationData.localFolder;
-    var filename = "checkins.csv";
+    if (secureID) {
+        secureID = "," + secureID.toLowerCase() + "\n";
+    }
 
     return localFolder.createFileAsync(filename, Windows.Storage.CreationCollisionOption.openIfExists)
         .then(function (file) {
-            return Windows.Storage.FileIO.appendTextAsync(file, personInfo + "\n");
-        }).done();
+            return Windows.Storage.FileIO.readTextAsync(file)
+                .then(function (fileContents) {
+                    fileContents = fileContents.toLowerCase();
+                    if ((personID && fileContents.indexOf(personID) >= 0) || (secureID && fileContents.indexOf(secureID) >= 0)) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                });
+        });
+}
+
+function saveCheckin(personID, secureID) {
+    //if already checked in don't save again
+    checkedInStatus(personID, secureID).then(function (isCheckedIn) {
+        if (isCheckedIn) {
+            return;
+        } else {
+            var personInfo = personID + ",";
+
+            if (secureID) {
+                personInfo += secureID;
+            }
+
+            return localFolder.createFileAsync(filename, Windows.Storage.CreationCollisionOption.openIfExists)
+                .then(function (file) {
+                    return Windows.Storage.FileIO.appendTextAsync(file, personInfo + "\n");
+                }).done();
+        }
+
+    });
 }
